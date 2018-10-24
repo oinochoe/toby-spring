102fcacab98e6048ec5ee369f02ed2722e5ba7ba
oinochoe <oinochoe@naver.com>
2018-10-24 23:44:48 +0900

    [김영민] 6장 AOP ... 6.1.1 메소드 분리 - 6-2 비즈니스 로직과 트랜잭션 경계설정의 분리

--- src/main/java/com/spring/springbook/user/service/UserService.java
+++ src/main/java/com/spring/springbook/user/service/UserService.java
@@ -36,16 +36,20 @@ public class UserService {
         TransactionStatus status =
                 this.transactionManager.getTransaction(new DefaultTransactionDefinition());
         try {
-            List<User> users = userDao.getAll();
-            for (User user : users) {
-                if (canUpgradeLevel(user)) {
-                    upgradeLevel(user);
-                }
-            }
+            upgradeLevelsInternal();
             this.transactionManager.commit(status);
         } catch (RuntimeException e) {
             this.transactionManager.rollback(status);
             throw e;
+        }
+    }
+
+    private void upgradeLevelsInternal() {
+        List<User> users = userDao.getAll();
+        for (User user : users) {
+            if (canUpgradeLevel(user)) {
+                upgradeLevel(user);
+            }
         }
     }
 

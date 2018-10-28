9269bcd988ad530bae8bc8cd0e4389b9f20803a1
oinochoe <oinochoe@naver.com>
2018-10-28 22:28:54 +0900

    [김영민] 6장 오류 수정 완료 - 오류 리스트 - UserDaoTest   * duplciateKey   * sqlExceptionTranslate - UserServiceTest   * readOnlyTransactionAttribute

--- src/test/java/com/spring/springbook/user/service/UserServiceTest.java
+++ src/test/java/com/spring/springbook/user/service/UserServiceTest.java
@@ -33,40 +33,9 @@ import com.spring.springbook.user.dao.UserDao;
 import com.spring.springbook.user.domain.Level;
 import com.spring.springbook.user.domain.User;
 
-import static org.hamcrest.CoreMatchers.is;
-import static org.junit.Assert.assertThat;
-import static org.junit.Assert.fail;
-import static org.mockito.Matchers.any;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
-import static com.spring.springbook.user.service.UserServiceImpl.MIN_LOGCOUNT_FOR_SILVER;
-import static com.spring.springbook.user.service.UserServiceImpl.MIN_RECCOMEND_FOR_GOLD;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
-import org.junit.Before;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.mockito.ArgumentCaptor;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationContext;
 import org.springframework.dao.TransientDataAccessResourceException;
-import org.springframework.mail.MailException;
-import org.springframework.mail.MailSender;
-import org.springframework.mail.SimpleMailMessage;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.transaction.PlatformTransactionManager;
-import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
-import com.spring.springbook.user.dao.UserDao;
-import com.spring.springbook.user.domain.Level;
-import com.spring.springbook.user.domain.User;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations="/test-applicationContext.xml")

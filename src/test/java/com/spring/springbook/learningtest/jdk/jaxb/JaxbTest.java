64b0fc32e8c2e4b2f0774d3a7b1719362d100099
oinochoe <oinochoe@naver.com>
2018-11-11 19:24:07 +0900

    [김영민] 7장 스프링 핵심 기술의 응용 - xjc -p com.spring.springbook.user.service.jaxb sqlmap.xsd -d src(jaxb api compile 명령)

--- src/test/java/com/spring/springbook/learningtest/jdk/jaxb/JaxbTest.java
+++ src/test/java/com/spring/springbook/learningtest/jdk/jaxb/JaxbTest.java
@@ -30,5 +30,12 @@ public class JaxbTest {
             List<SqlType> sqlList = sqlmap.getSql();
 
             assertThat(sqlList.size(), is(3));
+            assertThat(sqlList.get(0).getKey(), is("add"));
+            assertThat(sqlList.get(0).getValue(), is("insert"));
+            assertThat(sqlList.get(1).getKey(), is("get"));
+            assertThat(sqlList.get(1).getValue(), is("select"));
+            assertThat(sqlList.get(2).getKey(), is("delete"));
+            assertThat(sqlList.get(2).getValue(), is("delete"));
+            // List에 담겨있는 Sql 오브젝트를 가져와 XML문서와 같은 정보를 갖고 있는지 확인~~~
     }
 }
